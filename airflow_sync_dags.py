cb8a5934f3f58942dd5cdce71c47a0c928c38a40
Ignntbc <you@example.com>
2026-02-03 13:59:46 +0300

    Update list_folders to include all relevant directories for deployment

--- airflow_sync_dags.py
+++ airflow_sync_dags.py
@@ -47,7 +47,7 @@ ext_map = {
 }
 
 size_airflow_deploy = int(os.popen("du -s app/airflow_deploy | cut -f1").read())
-list_folders = ["dags"]#TODO вернуть все папки ,"csv", "jar", "keys", "keytab", "scripts", "user_data"
+list_folders = ["dags","csv", "jar", "keys", "keytab", "scripts", "user_data"]#TODO вернуть все папки 
 
 LOCAL_TEST = True
 if LOCAL_TEST:
